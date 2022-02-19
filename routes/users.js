8f0ca471a60d9284d85f9ca98c4c31b8c24e07a3
Joey <jtroinacoding@gmail.com>
2022-02-19 03:09:20 -0500

    pausing this project

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,24 @@
+const req = require("express/lib/request");
 const router = require("express").Router();
+const bcrypt = require("bcrypt")
 
-router.get("/", (req,res) => {
-    res.send("hey its user route")
+// update user
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        if (req.body.password){
+            try {
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }
+        }
+    } else {
+        return res.status(403).json("You can update only your account!")
+    }
 })
+
+// delete user
+// get a user
+// follow a user
+// unfollow
 
 module.exports = router
